9c57fe77d2b2328784b16bffb141816aa2cc03ab
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-02 12:39:09 +0200

    Create bash_pré-installation.sh

---
+++ bash_pré-installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install docker.io -y
+sudo apt install docker-compose -y?
+sudo apt install python3 venv -y ?
+sudo apt install python3-pip -y ?
+sudo app install git -y
+sudo git init
+sudo git clone github.com/jb3ducret3/inventaireDB
+mkdir inventaireDB
+docker build -t inventaireDB debian:latest .
+chmod +x bash_demarrage.sh
+./bash_demarrage.sh
