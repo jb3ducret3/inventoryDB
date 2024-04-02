9507972431aa70a9e4a9652cfc1c1e673f0d426a
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-02 12:39:55 +0200

    Update venv+lancement.sh

--- venv+lancement.sh
+++ venv+lancement.sh
@@ -2,7 +2,6 @@
 
 # Installer python3-venv si ce n'est pas déjà fait
 sudo apt-get update
-sudo apt-get install python3-venv
 
 # Créer un nouvel environnement virtuel
 python3 -m venv VenDB
@@ -18,4 +17,4 @@ pip install reportlab
 sudo apt-get install tree
 
 # Lancer le script Python
-python3 inventory.py
+docker-compose up -d
