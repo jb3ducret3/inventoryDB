eb95604937119751a997154d03b5812560a3e3be
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-03 09:33:37 +0200

    Update venv+lancement.sh

--- venv+lancement.sh
+++ venv+lancement.sh
@@ -1,8 +1,15 @@
-#!bin/bash
+#!/bin/bash
+sudo apt-get update
+# Installation de dnsmasq
+sudo apt-get install dnsmasq -y
+# Configuration de dnsmasq
+echo "address=/local/$PRIVATE_IP" | sudo tee -a /etc/dnsmasq.conf
 cd inventaireDB
 python3 -m venv venv
 source venv/bin/activate
 docker build -t inventaireDB debian:latest .
 export psswd=$(docker run --rm --name apache httpd:alpine htpasswd -nb admin corsic)
 export PRIVATE_IP=$(ip route get 1 | awk '{print $NF;exit}'):8888
+
+sudo systemctl restart dnsmasq
 docker-compose up -d
