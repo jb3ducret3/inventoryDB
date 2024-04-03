0f4dc8b25cb46fe063fd759ba7502d12224e8dbd
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-03 09:12:48 +0200

    Update venv+lancement.sh

--- venv+lancement.sh
+++ venv+lancement.sh
@@ -4,4 +4,5 @@ python3 -m venv venv
 source venv/bin/activate
 docker build -t inventaireDB debian:latest .
 export psswd=$(docker run --rm --name apache httpd:alpine htpasswd -nb admin corsic)
+export PRIVATE_IP=$(ip route get 1 | awk '{print $NF;exit}'):8888
 docker-compose up -d
