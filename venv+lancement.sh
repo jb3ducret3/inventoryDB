eb868ef62bdf060617dbcc72915c3b763ef0a61d
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-03 09:02:21 +0200

    Update venv+lancement.sh

--- venv+lancement.sh
+++ venv+lancement.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 cd inventaireDB
+python3 -m venv venv
+source venv/bin/activate
 export psswd=$(docker run --rm --name apache httpd:alpine htpasswd -nb admin corsic)
 docker build -t inventaireDB debian:latest .
 docker-compose up -d
