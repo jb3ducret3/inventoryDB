5226d5acebbc239e76896a4186bbad0e445ae277
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-03 08:57:19 +0200

    Update venv+lancement.sh

--- venv+lancement.sh
+++ venv+lancement.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install docker.io -y
-sudo apt install docker-compose -y?
-sudo apt install python3 venv -y ?
-sudo apt install python3-pip -y ?
-sudo app install git -y
-sudo git init
-sudo git clone github.com/jb3ducret3/inventaireDB
+#!bin/bash
 mkdir inventaireDB
 export psswd=$(docker run --rm --name apache httpd:alpine htpasswd -nb admin corsic)
 docker build -t inventaireDB debian:latest .
