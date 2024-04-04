5fa47b456e74de46124c05b5393faa6672ce58c9
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-04 09:28:57 +0200

    Create dnsmasq.sh

---
+++ dnsmasq.sh
@@ -0,0 +1,24 @@
+#!bin/bash  
+sudo apt update
+sudo apt upgrade
+sudo apt install dsnmasq -y
+systemctl disable --now systemd-resolved
+rm -rf /etc/resolv.conf # ==> prkoi ?
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+apt-get install dnsmasq dnsutils ldnsutils -y
+systemctl status dnsmasq
+nano /etc/dnsmasq.conf  #dnsmasq /!\
+port=53
+domain-needed
+bogus-priv
+listen-address=127.0.0.1,$ip_local
+expand-hosts
+domain=inventaire.local
+cache-size=1000
+nano /etc/resolv.conf
+nameserver $ip_local
+dnsmasq --test
+systemctl restart dnsmasq
+nano /etc/hosts
+$ip_local inventairedb.local
+dig inventaire.local+short
