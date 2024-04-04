581dad58fb43df15c04b3c246b14053529ae8275
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-04 07:00:42 +0200

    Update venvlancement.sh

--- venvlancement.sh
+++ venvlancement.sh
@@ -3,9 +3,9 @@ sudo apt-get update
 # Installation de dnsmasq
 export psswd=$(docker run --rm --name apache httpd:alpine htpasswd -nb admin techosgringos)
 export PRIVATE_IP=$(ip route get 1 | awk '{print $NF;exit}'):8888
-sudo apt-get install dnsmasq -y
+#sudo apt-get install dnsmasq -y
 # Configuration de dnsmasq
-echo "address=/local/$PRIVATE_IP" | sudo tee -a /etc/dnsmasq.conf
+#echo "address=/local/$PRIVATE_IP" | sudo tee -a /etc/dnsmasq.conf 
 docker build -t inventairedb .
 sudo systemctl restart dnsmasq
 docker-compose up -d
