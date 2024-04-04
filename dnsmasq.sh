7231cabc38079ccf16e83b13b1701e829905ee15
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-04 10:46:47 +0200

    Update dnsmasq.sh

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -3,24 +3,21 @@ sudo apt update
 sudo apt upgrade
 sudo apt install dsnmasq -y
 systemctl disable --now systemd-resolved
-rm -rf /etc/resolv.conf # ==> prkoi ?
+# rm -rf /etc/resolv.conf # ==> prkoi ?
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 apt-get install dnsmasq dnsutils ldnsutils -y
 systemctl status dnsmasq
-nano /etc/dnsmasq.conf  #dnsmasq /!\
-port=53
-domain-needed
-bogus-priv
-listen-address=127.0.0.1,$ip_local
-expand-hosts
-domain=inventaire.local
-cache-size=1000
-nano /etc/resolv.conf
-nameserver $ip_local
-dnsmasq --test
+echo "port=53
+      domain-needed
+      bogus-priv
+      listen-address=127.0.0.1,$ip_local
+      expand-hosts
+      domain=inventaire.local
+      cache-size=1000" > /etc/dnsmasq.conf  #dnsmasq /!\
+echo " nameserver $ip_local
+       dnsmasq --test" > /etc/resolv.conf
 systemctl restart dnsmasq
-nano /etc/hosts
-$ip_local inventairedb.local
+echo "$ip_local inventairedb.local" > /etc/hosts
 dig inventaire.local+short
 drill google.com | grep "Query time"
 
