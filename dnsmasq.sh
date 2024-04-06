5d0c063ba1240faf0f1b47f19c733109922c343f
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-06 12:54:04 +0200

    Update dnsmasq.sh

--- dnsmasq.sh
+++ dnsmasq.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
-
+ip_local=$(hostname -I | awk '{print $1}')
 # Mise à jour des paquets
 sudo apt update
-sudo apt upgrade
-
-# Installation de dnsmasq
-sudo apt install dnsmasq -y
-
-# Désactivation et arrêt de systemd-resolved
-systemctl disable --now systemd-resolved
-
-# Configuration du fichier /etc/resolv.conf avec Google DNS (8.8.8.8)
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
-echo "inventairedb.localhost 192.168.1.76" > /etc/resolv.conf
+sudo apt upgrade -y
 # Installation de dnsmasq, dnsutils et ldnsutils
-apt-get install dnsmasq dnsutils ldnsutils -y
-
-# Vérification du statut de dnsmasq
-systemctl status dnsmasq
+sudo apt install dnsmasq dnsutils ldnsutils -y
+# Désactivation et arrêt de systemd-resolved
+sudo systemctl disable --now systemd-resolved
+# Configuration du fichier /etc/resolv.conf avec Google DNS (8.8.8.8)
+echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
 
 # Configuration de dnsmasq
 echo "port=53
@@ -25,15 +16,15 @@ echo "port=53
       bogus-priv
       listen-address=127.0.0.1,$ip_local
       expand-hosts
-      domain=inventaire.local
-      cache-size=1000" > /etc/dnsmasq.conf
+      domain=inventairedb.localhost
+      cache-size=1000" | sudo tee /etc/dnsmasq.conf
 
-
-# Redémarrage de dnsmasq
-systemctl restart dnsmasq
 
 # Ajout de l'entrée pour inventairedb.local dans /etc/hosts
-echo "$ip_local inventairedb.localhost" >> /etc/hosts
+echo "$ip_local inventairedb.localhost" | sudo tee -a /etc/hosts
+
+# Redémarrage de dnsmasq
+sudo systemctl restart dnsmasq
 
 # Vérification du temps de réponse pour une requête DNS vers google.com
 drill google.com | grep "Query time"
