3aa1e2ffc86be67f393ae5f7cf2a0fbc1e2679bd
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-06 14:39:43 +0200

    Update lancement-conteneur.sh

--- lancement-conteneur.sh
+++ lancement-conteneur.sh
@@ -4,7 +4,6 @@ sudo apt-get update
 export psswd=$(docker run --rm --name apache httpd:alpine htpasswd -nb admin techosgringos)
 export PRIVATE_IP=$(ip route get 1 | awk '{print $NF;exit}'):8888 
 docker build -t inventairedb .
-sudo systemctl restart dnsmasq
 docker-compose up -d
 chmod +x dnsmasq.sh
 ./dnsmasq.sh
