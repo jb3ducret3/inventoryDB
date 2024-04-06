7861e564c3178dc147ccbf51ebecce32a4048f02
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-06 10:32:07 +0200

    Update dnsmasq_client.sh

--- dnsmasq_client.sh
+++ dnsmasq_client.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
+# Mise à jour des paquets
 sudo apt update
 sudo apt upgrade
-apt-get install dnsutils ldnsutils -y
-nano /etc/resolv.conf
-$IP_DISTANT inventaire.local
+
+# Installation de dnsutils et ldnsutils
+sudo apt-get install dnsutils ldnsutils -y
+
+# Modification du fichier /etc/resolv.conf pour ajouter l'adresse IP distante et le domaine inventaire.local
+echo "$IP_DISTANT inventaire.local" >> /etc/resolv.conf
+
+# Vérification de la résolution DNS pour inventaire.local
 dig inventaire.local
