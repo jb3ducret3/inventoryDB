240afaa45fdc074f94969a16aab34f4581a2ffb8
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-06 10:34:47 +0200

    Update element-de-base.sh

--- element-de-base.sh
+++ element-de-base.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
+ip_local=$(hostname -I | awk '{print $1}')
 sudo apt install docker.io -y
 sudo apt install docker-compose -y
 sudo apt-get install python3-venv -y 
