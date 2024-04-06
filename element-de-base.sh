3d4c74a9f6a2222442efc3ac308efed5cd2c5bcf
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-06 10:19:15 +0200

    Update element-de-base.sh

--- element-de-base.sh
+++ element-de-base.sh
@@ -5,9 +5,5 @@ sudo apt install docker.io -y
 sudo apt install docker-compose -y
 sudo apt-get install python3-venv -y 
 sudo apt install python3-pip -y 
-#sudo app install git -y
-#sudo git init
-#sudo git clone github.com/jb3ducret3/inventaireDB
-#cd inventaireDB
-chmod +x venvlancement.sh
-./venvlancement.sh
+chmod +x lancement-conteneur.sh
+./lancement-conteneur.sh
