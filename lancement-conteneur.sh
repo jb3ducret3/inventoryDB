0c26d59e6930b69101f7989b2eb6e51040e9406a
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-07 11:42:11 +0200

    Delete lancement-conteneur.sh

--- lancement-conteneur.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-# Installation de dnsmasq
-export psswd=$(docker run --rm --name apache httpd:alpine htpasswd -nb admin techosgringos)
-export PRIVATE_IP=$(ip route get 1 | awk '{print $NF;exit}'):8888 
-docker build -t inventairedb .
-docker-compose up -d
-chmod +x dnsmasq.sh
-./dnsmasq.sh
