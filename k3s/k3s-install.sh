f517dc97b147360f73feaea20ec310b4566a155a
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-09 13:46:34 +0200

    Update k3s-install.sh

--- k3s/k3s-install.sh
+++ k3s/k3s-install.sh
@@ -5,6 +5,7 @@ curl -sfL https://get.k3s.io | sh -
 cat
 K3S_TOKEN:$
 K3S_URL:$10.10.10.10:6443
+node1=$10.10.10.20
 ssh worker@node1 curl -sfL https://get.k3s.io | K3S_URL=https://myserver:6443 K3S_TOKEN=mynodetoken sh -
 cd /home/inventairedb/
 curl -L https://github.com/kubernetes/kompose/releases/download/v1.16.0/kompose-linux-amd64 -o kompose
@@ -25,3 +26,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+https://medium.com/47billion/playing-with-kubernetes-using-k3d-and-rancher-78126d341d23 K3D.
+https://cours.brosseau.ovh/tp/ci/kubernetes/deploy-container-in-kubernetes.html
+https://www.sokube.io/blog/k3d-k3s-k8s-perfect-match-for-dev-and-testing
