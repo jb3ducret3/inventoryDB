b8a043cdb0aaaf733556ec277f2c2bd322f556d5
jb3ducret3 <141333593+jb3ducret3@users.noreply.github.com>
2024-04-14 09:36:44 +0200

    Update k3s-install.sh

--- k3s/k3s-install.sh
+++ k3s/k3s-install.sh
@@ -7,44 +7,34 @@ sudo apt install curl -y
 sudo apt install openssh-client -y
 
 # Installation de K3s
+# Télécharge et installe K3s
 curl -sfL https://get.k3s.io | sh -
+
+# Récupération de l'adresse IP locale de l'interface réseau principale (eth0)
+K3S_IP=$(ip -o -4 addr show eth0 | awk '{print $4}' | cut -d "/" -f 1)
+# Crée l'URL pour accéder à K3s
+K3S_URL="http://$K3S_IP:6443"
+# Récupère le jeton d'authentification K3s
 K3S_TOKEN=$(cat /var/lib/rancher/k3s/server/token)
-K3S_URL="http://monip:6443"
 
-# Déploiement des nœuds ouvriers
-#ssh worker@node1 "curl -sfL https://get.k3s.io | K3S_URL=$K3S_URL K3S_TOKEN=$K3S_TOKEN sh -"
+# Demande à l'utilisateur de saisir son nom d'utilisateur Docker et son mot de passe
+read -p "Entrez votre nom d'utilisateur Docker : " DCK_LOG
+read -sp "Entrez votre mot de passe Docker : " DCK_MDP
+echo # Nouvelle ligne pour améliorer la lisibilité
 
-# Installation de Kompose
-curl -L https://github.com/kubernetes/kompose/releases/download/v1.16.0/kompose-linux-amd64 -o kompose
-#chmod +x kompose
-#sudo mv kompose /usr/local/bin/kompose
+# Se connecte à Docker Hub avec les informations d'identification fournies par l'utilisateur
+docker login -u "$DCK_LOG" -p "$DCK_MDP"
 
-# Vérification de l'application déployée
-curl inventairedb.localhost
+# Tag et pousse les images Docker vers le référentiel Docker Hub de l'utilisateur
+docker tag dnsserveur:latest "$DCK_LOG/projet:dnsserveur"
+docker push "$DCK_LOG/projet:dnsserveur"
 
-if [ $? -ne 0 ]; then
-    echo "Erreur lors de la vérification de l'application déployée."
-    exit 1
-fi
+docker tag inventairedb:latest "$DCK_LOG/projet:inventairedb"
+docker push "$DCK_LOG/projet:inventairedb"
 
-# Nom du référentiel sur Docker Hub
-repository="userdockerhub/repository"
-
-# Liste des images à tagger et à pousser
-images=("inventairedb" "dnsserveur")
-
-# Parcourir la liste des images
-for image in "${images[@]}"; do
-    # Tagger l'image avec le nom du référentiel et le tag correspondant
-    docker tag "$image:latest" "$repository:$image"
-    
-    # Pousser l'image taggée vers Docker Hub
-    docker push "$repository:$image"
-done
-
-# Application des manifestes Kubernetes
+# Applique les manifestes Kubernetes pour déployer les services
 kubectl apply -f inventairedb.yml
 
+# Ajoute des annotations aux déploiements Kubernetes pour configurer Traefik
 kubectl annotate deployment inventairedb traefik.ingress.kubernetes.io/router.entrypoints=web
 kubectl annotate deployment inventairedb traefik.ingress.kubernetes.io/router.rule="Host(`inventairedb.localhost`)"
-
